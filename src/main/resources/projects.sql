7959de8edbf1ae224bc27efa96416eb56ecefb59
Thanusree19 <123344735+Thanusree19@users.noreply.github.com>
2023-04-20 11:57:00 +0530

    Update projects.sql

--- src/main/resources/projects.sql
+++ src/main/resources/projects.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO PROJECT ( ID, NAME, DESCRIPTION)
 VALUES
-(1, 'Azure',	'Azure Project'),
-(2, 'Jenkins',	'Jenkins Project'),
-(3, 'Kubernetes',	'Kubernetes Project');
+(1, 'Azure',	'Azure Project1'),
+(2, 'Jenkins',	'Jenkins Project1'),
+(3, 'Kubernetes',	'Kubernetes Project1');
